5fc2545b2b25288d50f20d1b75c6deec4bd9594c
ChenmoshaoAlen <chenmoshao@gmail.com>
2023-06-10 09:36:47 +0800

    first commit

---
+++ StationLinearPath.cpp
@@ -0,0 +1,52 @@
+/**
+  ******************************************************************************
+  * Copyright (c) 2019 - ~, SCUT-RobotLab Development Team
+  * @file    StationLinearPath.cpp
+  * @author  ZengXilang chenmoshaoalen@126.com
+  * @brief   
+  * @date    2023/06/10 09:33:41
+  * @version 1.0
+  * @par Change Log:
+  * <table>
+  * <tr><th>Date <th>Version <th>Author <th>Description
+  * <tr><td>2022-11-01 <td> 1.0 <td>S.B. <td>Creator
+  * </table>
+  *
+  ==============================================================================
+                               How to use this Lib  
+  ==============================================================================
+    @note
+      -# 
+      -# 
+    @warning
+      -# 
+      -# 
+  ******************************************************************************
+  * @attention
+  * 
+  * if you had modified this file, please make sure your code does not have many
+  * bugs, update the version Number, write dowm your name and the date, the most
+  * important is make sure the users will have clear and definite understanding 
+  * through your new brief.
+  *
+  * <h2><center>&copy; Copyright (c) 2019 - ~, SCUT-RobotLab Development Team.
+  * All rights reserved.</center></h2>
+  ******************************************************************************
+  */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "StationLinearPath.h"
+/* Private define ------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private type --------------------------------------------------------------*/
+
+/* Private function declarations ---------------------------------------------*/
+
+/* function prototypes -------------------------------------------------------*/
+
+
+
+/************************ COPYRIGHT(C) SCUT-ROBOTLAB **************************/
---
+++ StationLinearPath.h
@@ -0,0 +1,42 @@
+/**
+  ******************************************************************************
+  * Copyright (c) 2019 - ~, SCUT-RobotLab Development Team
+  * @file    StationLinearPath.h
+  * @author  ZengXilang chenmoshaoalen@126.com
+  * @brief   Header file 
+  ******************************************************************************
+  * @attention
+  * 
+  * if you had modified this file, please make sure your code does not have many
+  * bugs, update the version Number, write dowm your name and the date, the most
+  * important is make sure the users will have clear and definite understanding 
+  * through your new brief.
+  *
+  * <h2><center>&copy; Copyright (c) 2019 - ~, SCUT-RobotLab Development Team.
+  * All rights reserved.</center></h2>
+  ******************************************************************************
+  */
+#pragma once
+
+// #ifdef __cplusplus
+// extern "C"{
+// #endif
+/* Includes ------------------------------------------------------------------*/
+
+/* Private macros ------------------------------------------------------------*/
+
+/* Private type --------------------------------------------------------------*/
+
+/* Exported macros -----------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported function declarations --------------------------------------------*/
+
+
+
+// #ifdef  __cplusplus
+// }
+// #endif
+
+/************************ COPYRIGHT(C) SCUT-ROBOTLAB **************************/
