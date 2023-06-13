bedef8eb812b225a15fcc0ba7b125aa758008d03
ChenmoshaoAlen <chenmoshao@gmail.com>
2023-06-13 16:22:19 +0800

    添加了计算函数，轨迹点生成函数，限位检测函数，轨迹点获取函数

--- StationLinearPath.cpp
+++ StationLinearPath.cpp
@@ -16,7 +16,7 @@
                                How to use this Lib  
   ==============================================================================
     @note
-      -# 
+      -# 云台下方地面为原点，吸盘方向为x，竖直向上为z，建立机器人坐标系
       -# 
     @warning
       -# 
@@ -71,17 +71,73 @@ void SLPClassdef::quaCoord2TMatrix(float qx, float qy, float qz, float qw, float
  * 
  * @param visionPack 
  */
-void SLPClassdef::recVisionTarget(VisionPackStructdef visionPack)
+void SLPClassdef::recVisionTarget(VisionPackStructdef &visionPack)
 {
   quaCoord2TMatrix(visionPack.qx,visionPack.qy,visionPack.qz,visionPack.qw,visionPack.x,visionPack.y,visionPack.z,visionT);
   TWorldGoal[0][0] = visionT[2][2];   TWorldGoal[0][1] = -visionT[2][0];    TWorldGoal[0][2] = -visionT[2][1];     TWorldGoal[0][3] = visionT[2][3];
   TWorldGoal[1][0] = -visionT[0][2];   TWorldGoal[1][1] = visionT[0][0];    TWorldGoal[1][2] = visionT[0][1];     TWorldGoal[1][3] = -visionT[0][3];
   TWorldGoal[2][0] = -visionT[1][2];   TWorldGoal[2][1] = visionT[1][0];    TWorldGoal[2][2] = visionT[1][1];     TWorldGoal[2][3] = -visionT[1][3];
 
+  goalWorld[0] = TWorldGoal[0][3];
+  goalWorld[1] = TWorldGoal[1][3];
+  goalWorld[2] = TWorldGoal[2][3];
+
   TGoalWorld[0][0] = visionT[2][2];   TGoalWorld[0][1] = -visionT[0][2];    TGoalWorld[0][2] = -visionT[1][2];     TGoalWorld[0][3] = visionT[1][3] * visionT[0][2] - visionT[0][3] * visionT[2][2] + visionT[2][3] * visionT[1][2];
   TGoalWorld[1][0] = -visionT[2][0];   TGoalWorld[1][1] = visionT[0][0];    TGoalWorld[1][2] = visionT[1][0];     TGoalWorld[1][3] = visionT[0][3] * visionT[2][0] - visionT[1][3] * visionT[0][0] - visionT[2][3] * visionT[1][0];
   TGoalWorld[2][0] = -visionT[2][1];   TGoalWorld[2][1] = visionT[0][1];    TGoalWorld[2][2] = visionT[1][1];     TGoalWorld[2][3] = visionT[0][3] * visionT[2][1] - visionT[1][3] * visionT[0][1] - visionT[2][3] * visionT[1][1];
 }
+
+
+/**
+ * @brief 计算轨迹点
+ * 
+ * @return true 
+ * @return false 
+ */
+bool SLPClassdef::Calculate()
+{
+  /* 判断终点是否超限 */
+  xyzTracGene(goalWorld, GoalxyzTrac[0], GoalxyzTrac[1], GoalxyzTrac[2]);
+  if (false == limitCheck(GoalxyzTrac[0], GoalxyzTrac[1], GoalxyzTrac[2]))
+    return false;
+
+  /* 计算中间点 */
+  endEffLocCal();
+  decSurfaceCal(endEffGoal);
+  midPointCal();
+  xyzTracGene(midWorld,MidxyzTrac[0],MidxyzTrac[1],MidxyzTrac[2]);
+  /* 判断中间点是否超限 */
+  return  limitCheck(MidxyzTrac[0], MidxyzTrac[1], MidxyzTrac[2]);
+}
+
+/**
+ * @brief 获取中间点
+ * 
+ * @param lift 
+ * @param extend 
+ * @param translate 
+ */
+void SLPClassdef::getMidxyzTrac(float &lift, float &extend, float &translate)
+{
+  lift = MidxyzTrac[0];
+  extend = MidxyzTrac[1];
+  translate = MidxyzTrac[2];
+}
+
+/**
+ * @brief 获取目标点
+ * 
+ * @param lift 
+ * @param extend 
+ * @param translate 
+ */
+void SLPClassdef::getGoalxyzTrac(float &lift, float &extend, float &translate)
+{
+  lift = GoalxyzTrac[0];
+  extend = GoalxyzTrac[1];
+  translate = GoalxyzTrac[2];
+}
+
 
 /**
  * @brief 解算出小三轴末端相对于兑换站的xyz位置
@@ -93,8 +149,8 @@ void SLPClassdef::recVisionTarget(VisionPackStructdef visionPack)
 void SLPClassdef::endEffLocCal(float yaw, float pitch, float roll)
 {
   /* 考虑小三轴位移 */
-
-  /* 不考虑小三轴位移 */
+  /* code */
+  /* 不考虑小三轴位移 TODO应该根据动作组移动到一个确定的位置 */
   endEffWorld[0] = 1;
   endEffWorld[1] = 1;
   endEffWorld[2] = 1;
@@ -108,42 +164,42 @@ void SLPClassdef::endEffLocCal(float yaw, float pitch, float roll)
  * @brief 通过决策面计算小三轴
  * 
  * @param endEffGoal 
- * @return uint8_t -1：无解，0：前表面，1：上表面，2：左表面，3：右表面，4：下表面
+ * @return uint8_t 0：无解，1：前表面，2：上表面，3：左表面，4：右表面，5：下表面
  */
 uint8_t SLPClassdef::decSurfaceCal(float endEffGoal[3])
 {
   if (endEffGoal[0] + safeR < 0)        //前表面
-          return 0;
+          return 1;
   else if (endEffGoal[0] + safeR >= 0   //上表面
         && ((R[1]-G[1])*(U[2]-G[2])-(R[2]-G[2])*(U[1]-G[1]))*(endEffGoal[0]-G[0]) - ((R[0]-G[0])*(U[2]-G[2])-(R[2]-G[2])*(U[0]-G[0]))*(endEffGoal[1]-G[1]) + ((R[0]-G[0])*(U[1]-G[1])-(R[1]-G[1])*(U[0]-G[0]))*(endEffGoal[2]-G[2]) <= 0
         && ((R[1]-G[1])*(S[2]-G[2])-(R[2]-G[2])*(S[1]-G[1]))*(endEffGoal[0]-G[0]) - ((R[0]-G[0])*(S[2]-G[2])-(R[2]-G[2])*(S[0]-G[0]))*(endEffGoal[1]-G[1]) + ((R[0]-G[0])*(S[1]-G[1])-(R[1]-G[1])*(S[0]-G[0]))*(endEffGoal[2]-G[2]) >= 0
         && ((T[1]-G[1])*(U[2]-G[2])-(T[2]-G[2])*(U[1]-G[1]))*(endEffGoal[0]-G[0]) - ((T[0]-G[0])*(U[2]-G[2])-(T[2]-G[2])*(U[0]-G[0]))*(endEffGoal[1]-G[1]) + ((T[0]-G[0])*(U[1]-G[1])-(T[1]-G[1])*(U[0]-G[0]))*(endEffGoal[2]-G[2]) >= 0)
         {
-          return 1;
+          return 2;
         }
   else if (endEffGoal[0] + safeR >= 0   //左表面
         && ((R[1]-G[1])*(S[2]-G[2])-(R[2]-G[2])*(S[1]-G[1]))*(endEffGoal[0]-G[0]) - ((R[0]-G[0])*(S[2]-G[2])-(R[2]-G[2])*(S[0]-G[0]))*(endEffGoal[1]-G[1]) + ((R[0]-G[0])*(S[1]-G[1])-(R[1]-G[1])*(S[0]-G[0]))*(endEffGoal[2]-G[2]) < 0
         && ((T[1]-G[1])*(U[2]-G[2])-(T[2]-G[2])*(U[1]-G[1]))*(endEffGoal[0]-G[0]) - ((T[0]-G[0])*(U[2]-G[2])-(T[2]-G[2])*(U[0]-G[0]))*(endEffGoal[1]-G[1]) + ((T[0]-G[0])*(U[1]-G[1])-(T[1]-G[1])*(U[0]-G[0]))*(endEffGoal[2]-G[2]) >= 0
         && ((R[1]-G[1])*(O[2]-G[2])-(R[2]-G[2])*(O[1]-G[1]))*(endEffGoal[0]-G[0]) - ((R[0]-G[0])*(O[2]-G[2])-(R[2]-G[2])*(O[0]-G[0]))*(endEffGoal[1]-G[1]) + ((R[0]-G[0])*(O[1]-G[1])-(R[1]-G[1])*(O[0]-G[0]))*(endEffGoal[2]-G[2]) >= 0)
         {
-          return 2;
+          return 3;
         }
   else if (endEffGoal[0] + safeR >= 0   //右表面
         && ((R[1]-G[1])*(S[2]-G[2])-(R[2]-G[2])*(S[1]-G[1]))*(endEffGoal[0]-G[0]) - ((R[0]-G[0])*(S[2]-G[2])-(R[2]-G[2])*(S[0]-G[0]))*(endEffGoal[1]-G[1]) + ((R[0]-G[0])*(S[1]-G[1])-(R[1]-G[1])*(S[0]-G[0]))*(endEffGoal[2]-G[2]) >= 0
         && ((T[1]-G[1])*(U[2]-G[2])-(T[2]-G[2])*(U[1]-G[1]))*(endEffGoal[0]-G[0]) - ((T[0]-G[0])*(U[2]-G[2])-(T[2]-G[2])*(U[0]-G[0]))*(endEffGoal[1]-G[1]) + ((T[0]-G[0])*(U[1]-G[1])-(T[1]-G[1])*(U[0]-G[0]))*(endEffGoal[2]-G[2]) < 0
         && ((N[1]-G[1])*(U[2]-G[2])-(N[2]-G[2])*(U[1]-G[1]))*(endEffGoal[0]-G[0]) - ((N[0]-G[0])*(U[2]-G[2])-(N[2]-G[2])*(U[0]-G[0]))*(endEffGoal[1]-G[1]) + ((N[0]-G[0])*(U[1]-G[1])-(N[1]-G[1])*(U[0]-G[0]))*(endEffGoal[2]-G[2]) >= 0)
         {
-          return 3;
+          return 4;
         }
   else if (endEffGoal[0] + safeR >= 0   //下表面
         && ((R[1]-G[1])*(S[2]-G[2])-(R[2]-G[2])*(S[1]-G[1]))*(endEffGoal[0]-G[0]) - ((R[0]-G[0])*(S[2]-G[2])-(R[2]-G[2])*(S[0]-G[0]))*(endEffGoal[1]-G[1]) + ((R[0]-G[0])*(S[1]-G[1])-(R[1]-G[1])*(S[0]-G[0]))*(endEffGoal[2]-G[2]) < 0
         && ((T[1]-G[1])*(U[2]-G[2])-(T[2]-G[2])*(U[1]-G[1]))*(endEffGoal[0]-G[0]) - ((T[0]-G[0])*(U[2]-G[2])-(T[2]-G[2])*(U[0]-G[0]))*(endEffGoal[1]-G[1]) + ((T[0]-G[0])*(U[1]-G[1])-(T[1]-G[1])*(U[0]-G[0]))*(endEffGoal[2]-G[2]) < 0
         && ((O[1]-G[1])*(N[2]-G[2])-(O[2]-G[2])*(N[1]-G[1]))*(endEffGoal[0]-G[0]) - ((O[0]-G[0])*(N[2]-G[2])-(O[2]-G[2])*(N[0]-G[0]))*(endEffGoal[1]-G[1]) + ((O[0]-G[0])*(N[1]-G[1])-(O[1]-G[1])*(N[0]-G[0]))*(endEffGoal[2]-G[2]) >= 0)
         {
-          return 4;
+          return 5;
         }
   else                                  //无解
-          return -1;
+          return 0;
 }
 
 /**
@@ -151,48 +207,86 @@ uint8_t SLPClassdef::decSurfaceCal(float endEffGoal[3])
  * 
  * @param mid 
  */
-void SLPClassdef::midPointGenerate(float mid[3])
+void SLPClassdef::midPointCal()
 {
   uint8_t surfaceRes = decSurfaceCal(G);
   static float midGoal[3] = {0};
   switch (surfaceRes)
   {
-  case 0:       //前表面
+  case 1:       //前表面
     midGoal[0] = G[0];
     midGoal[1] = G[1];
     midGoal[2] = G[2];
     break;
-  case 1:       //上表面
+  case 2:       //上表面
     midGoal[0] = -safeR;
-    midGoal[1] = (endEffGoal[1] * (0.144 + safeR)) / (endEffGoal[2]);
-    midGoal[2] = 0.144 + safeR;
+    midGoal[1] = (endEffGoal[1] * (0.144f + safeR)) / (endEffGoal[2]);
+    midGoal[2] = 0.144f + safeR;
     break;
-  case 2:       //左表面
+  case 3:       //左表面
     midGoal[0] = -safeR;
-    midGoal[1] = 0.144 + safeR;
-    midGoal[2] = (endEffGoal[2] * (0.144 + safeR)) / (endEffGoal[1]);
+    midGoal[1] = 0.144f + safeR;
+    midGoal[2] = (endEffGoal[2] * (0.144f + safeR)) / (endEffGoal[1]);
     break;
-  case 3:       //右表面
+  case 4:       //右表面
     midGoal[0] = -safeR;
-    midGoal[1] = -0.144 - safeR;
-    midGoal[2] = (endEffGoal[2] * (-0.144 - safeR)) / (endEffGoal[1]);
+    midGoal[1] = -0.144f - safeR;
+    midGoal[2] = (endEffGoal[2] * (-0.144f - safeR)) / (endEffGoal[1]);
     break;
-  case 4:       //下表面
+  case 5:       //下表面
     midGoal[0] = -safeR;
-    midGoal[1] = (endEffGoal[1] * (-0.144 - safeR)) / (endEffGoal[2]);
-    midGoal[2] = -0.144 - safeR;
+    midGoal[1] = (endEffGoal[1] * (-0.144f - safeR)) / (endEffGoal[2]);
+    midGoal[2] = -0.144f - safeR;
     break;
   
   default:
     break;
   }
 
-  if(surfaceRes != -1)
+  if(surfaceRes != 0)
   {
-    mid[0] = TWorldGoal[0][0] * midGoal[0] + TWorldGoal[0][1] * midGoal[1] + TWorldGoal[0][2] * midGoal[2] + TWorldGoal[0][3];
-    mid[1] = TWorldGoal[1][0] * midGoal[0] + TWorldGoal[1][1] * midGoal[1] + TWorldGoal[1][2] * midGoal[2] + TWorldGoal[1][3];
-    mid[2] = TWorldGoal[2][0] * midGoal[0] + TWorldGoal[2][1] * midGoal[1] + TWorldGoal[2][2] * midGoal[2] + TWorldGoal[2][3];
+    midWorld[0] = TWorldGoal[0][0] * midGoal[0] + TWorldGoal[0][1] * midGoal[1] + TWorldGoal[0][2] * midGoal[2] + TWorldGoal[0][3];
+    midWorld[1] = TWorldGoal[1][0] * midGoal[0] + TWorldGoal[1][1] * midGoal[1] + TWorldGoal[1][2] * midGoal[2] + TWorldGoal[1][3];
+    midWorld[2] = TWorldGoal[2][0] * midGoal[0] + TWorldGoal[2][1] * midGoal[1] + TWorldGoal[2][2] * midGoal[2] + TWorldGoal[2][3];
   }
+}
+
+/**
+ * @brief 产生平移轨迹
+ * @note TODO常数项需要微调
+ * @param point 世界坐标系下的x--y--z
+ * @param lift 
+ * @param extend 
+ * @param translate 
+ * @return true 
+ * @return false 
+ */
+void SLPClassdef::xyzTracGene(float point[3], float &lift, float &extend, float &translate)
+{
+  lift = point[2] - 0.1f;
+  extend = point[0] - 0.5f;
+  translate = point[1] + 0.2f;
+}
+
+/**
+ * @brief 判断关节位置有无超限
+ * @note TODO 关节限位需要微调
+ * @param lift 
+ * @param extend 
+ * @param translate 
+ * @return true 
+ * @return false 
+ */
+bool SLPClassdef::limitCheck(float &lift, float &extend, float &translate)
+{
+  if(0.0f <= lift      && lift      <= 0.61f
+  && 0.0f <= extend    && extend    <= 0.339f
+  && 0.0f <= translate && translate <= 0.348f)
+  {
+    return true;
+  }
+  else
+    return false;
 }
 
 /************************ COPYRIGHT(C) SCUT-ROBOTLAB **************************/
--- StationLinearPath.h
+++ StationLinearPath.h
@@ -46,11 +46,13 @@ typedef struct _VisionPackStructdef
 class SLPClassdef
 {
 private:
-  float visionT[3][4];
-  float TWorldGoal[3][4];
-  float TGoalWorld[3][4];
-  float endEffWorld[3];
-  float endEffGoal[3];
+  float visionT[3][4];            //视觉旋转矩阵与位置
+  float TWorldGoal[3][4];         //世界坐标系描述兑换站坐标系
+  float TGoalWorld[3][4];         //兑换站坐标系描述世界坐标系
+  float endEffWorld[3];           // 末端小三轴世界坐标系xyz
+  float endEffGoal[3];            // 末端小三轴兑换站坐标系xyz
+  float midWorld[3];              // 中间点xyz
+  float goalWorld[3];              // 目标点xyz
   /* 在Goal坐标系下 */
   float O[3] = {0.288 + safeR, 0.144 + safeR, -0.144 - safeR};
   float P[3] = {0 - safeR, 0.144 + safeR, -0.144 - safeR};
@@ -62,22 +64,25 @@ private:
   float U[3] = {0.288 + safeR, -0.144 - safeR, 0.144 + safeR};
   float G[3] = {-safeR, 0, 0};    // 目标点xyz
   /* ↑↑在Goal坐标系下 */
-  float midWorld[3];                   // 中间点xyz
+  float MidxyzTrac[3];            //lift--extend--translate
+  float GoalxyzTrac[3];           //lift--extend--translate
 
   void quaCoord2TMatrix(float qx,float qy,float qz,float qw,float x,float y,float z,float TMat[3][4]);
-  void endEffLocCal(float yaw,float pitch,float roll);
-  bool limitCheck(float point[3]);
+  void endEffLocCal(float yaw = 0.0f, float pitch = 0.0f, float roll = 0.0f);
+  uint8_t decSurfaceCal(float endEffGoal[3]);
+  void midPointCal();
+  void xyzTracGene(float point[3],float &lift, float &extend, float &translate);
+  bool limitCheck(float &lift, float &extend, float &translate);
 public:
   float safeR = 0.2;
-  bool CalSuccess = false;
 
   SLPClassdef(){};
   ~SLPClassdef(){};
 
   void recVisionTarget(VisionPackStructdef &visionPack);
-  uint8_t decSurfaceCal(float endEffGoal[3]);
-  void midPointGenerate(float mid[3]);
-  bool xyzTracGenerate(float point[3],float &lift, float &extend, float &translate);
+  bool Calculate();
+  void getMidxyzTrac(float &lift, float &extend, float &translate);
+  void getGoalxyzTrac(float &lift, float &extend, float &translate);
 };
 
 
